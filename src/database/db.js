76289298aa2ce15739615516451042339abbc8d8
Eduardo Pereira <eduardop.boares@gmail.com>
2019-10-02 08:23:39 -0300

    consolelog env

--- src/database/db.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    DB: 'mongodb+srv://dev:dev@cluster0-uxdmg.mongodb.net/test?retryWrites=true&w=majority'
-}
--- src/database/index.js
+++ src/database/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 console.log('Conectando...')
 
+console.log(process.env.MONGO_URL);
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, autoIndex: false }).then(
     () => {console.log('\x1b[32m', 'Banco de Dados conectado com Sucesso', '\x1b[0m') },
     err => { console.log('\x1b[31m', 'Não é possível conectar ao Banco de Dados' + err, '\x1b[0m')}
